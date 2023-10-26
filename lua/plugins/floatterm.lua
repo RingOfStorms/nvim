ad0be82ef847618ca6172dd1cdc92efeed565223
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-10-26 02:53:56 -0500

    floaterm

---
+++ lua/plugins/floatterm.lua
@@ -0,0 +1,14 @@
+return {
+  "voldikss/vim-floaterm",
+  cmd = { "FloatermNew", "FloatermToggle" },
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>:'<,'>FloatermNew --autoclose=2<cr>",
+      desc = "Run selected as command in float terminal",
+      mode = "v"
+    },
+    { "<C-x>", "<cmd>FloatermToggle Terminal<cr>", desc = "Toggle float terminal" },
+    { "<C-z>", "<cmd>FloatermNew --disposable<cr>", desc = "Toggle disposable float terminal", mode = { "v", "n" }}
+  },
+}
