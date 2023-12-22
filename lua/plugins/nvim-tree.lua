aafcb2902e079d37059dd96130b97dffdeceddfb
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-12-22 16:28:10 -0600

    add back in a tree based file browser, all of these are useful in there own ways

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    sort = {
+      sorter = "case_sensitive",
+    },
+    view = {
+      width = 30,
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+  },
+  -- config = function()
+  --   require("nvim-tree").setup({})
+  -- end,
+  keys = {
+    { "<leader>e", "<cmd>NvimTreeToggle<cr>",          desc = "Open file browser" },
+    { "<leader>o", "<cmd>NvimTreeFindFile<cr>", desc = "Open file browser at current buffer" },
+  },
+}
--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -5,7 +5,7 @@ return {
     require("telescope").load_extension("file_browser")
   end,
   keys = {
-    { "<leader>e", "<cmd>Telescope file_browser<cr>", desc = "Open file browser" },
-    { "<leader>o", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", desc = "Open file browser" },
+    { "<leader>fd", "<cmd>Telescope file_browser<cr>", desc = "Open telescope file browser" },
+    { "<leader>fh", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", desc = "Open telescope file browser at current buffer" },
   },
 }
