9e25ee3a6ec27571ce4614aeddf683fa8d7a4db1
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-12-27 16:23:40 -0600

    resize nvmtree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,23 +4,38 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = 30,
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = false,
-      git_ignored = false,
-    },
-  },
+  opts = function()
+    local getWidth = function()
+      local w = vim.api.nvim_get_option("columns")
+      return math.ceil(w * 0.2)
+    end
+
+    vim.api.nvim_create_autocmd("VimResized", {
+      pattern = "*",
+      callback = function()
+        vim.cmd("NvimTreeResize " .. tostring(getWidth()))
+      end,
+    })
+
+    return {
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = getWidth(),
+      },
+      renderer = {
+        group_empty = true,
+        indent_width = 1,
+      },
+      filters = {
+        dotfiles = false,
+        git_ignored = false,
+      },
+    }
+  end,
   keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<cr>",          desc = "Open file browser" },
+    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Open file browser" },
     { "<leader>o", "<cmd>NvimTreeFindFile<cr>", desc = "Open file browser at current buffer" },
   },
 }
