bdb29f4e51abe48093713dc06fb1b52fcd7b9a6a
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2024-05-27 22:36:02 -0500

    change scroll off

--- lua/options.lua
+++ lua/options.lua
@@ -74,7 +74,7 @@ vim.diagnostic.config({
 })
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 3
 
 -- Turn on new diff
 vim.opt.diffopt:append("linematch:20")
