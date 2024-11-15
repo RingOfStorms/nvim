9f8756a8dd2fcd38ecbc3593bf8d7f2fe2748aa0
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2024-11-14 23:41:31 -0600

    update avante keys

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,30 +1,35 @@
 return {
-	-- cond = false,
 	"yetone/avante.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"stevearc/dressing.nvim",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		--- The below dependencies are optional,
-		-- {
-		--   -- support for image pasting
-		--   "HakonHarnes/img-clip.nvim",
-		--   event = "VeryLazy",
-		--   opts = {
-		--     -- recommended settings
-		--     default = {
-		--       embed_image_as_base64 = false,
-		--       prompt_for_file_name = false,
-		--       drag_and_drop = {
-		--         insert_mode = true,
-		--       },
-		--       -- required for Windows users
-		--       use_absolute_path = true,
-		--     },
-		--   },
-		-- },
-	},
+	dependencies = (function()
+		local deps = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			-- {
+			--   -- support for image pasting
+			--   "HakonHarnes/img-clip.nvim",
+			--   event = "VeryLazy",
+			--   opts = {
+			--     -- recommended settings
+			--     default = {
+			--       embed_image_as_base64 = false,
+			--       prompt_for_file_name = false,
+			--       drag_and_drop =
+			--         insert_mode = true,
+			--       },
+			--       -- required for Windows users
+			--       use_absolute_path = true,
+			--     },
+			--   },
+			-- },
+		}
+		-- Only add copilot if ANTHROPIC_API_KEY is not set
+		if os.getenv("ANTHROPIC_API_KEY") == nil then
+			table.insert(deps, "zbirenbaum/copilot.lua")
+		end
+		return deps
+	end)(),
 	event = "VeryLazy",
 	build = function()
 		-- TODO does this actually work? I still dont have full non nix support tested for this config.
@@ -43,10 +48,32 @@ return {
 
 		return {
 			provider = provider,
+			auto_suggestions_provider = provider,
+			hints = { enabled = false },
 			behavior = {
-				-- auto_suggestions = true, -- Experimental stage
+				auto_suggestions = true, -- Experimental stage
 				auto_set_keymaps = false,
 				support_paste_from_clipboard = true,
+				auto_apply_diff_after_generation = false,
+			},
+			windows = {
+				position = "top",
+				input = {
+					prefix = "",
+				},
+			},
+			mappings = {
+				ask = "<nop>",
+				edit = "<nop>",
+				refresh = "<leader><leader>r",
+				focus = "<leader><leader>f",
+				toggle = {
+					default = "<leader><leader>c",
+					debug = "<nop>",
+					hint = "<nop>",
+					suggestion = "<leader><leader>S",
+					repomap = "<leader><leader>R",
+				},
 			},
 		}
 	end,
@@ -59,18 +86,6 @@ return {
 			"<leader><leader>c",
 			"<cmd>AvanteToggle<cr>",
 			desc = "Avante - Toggle Chat",
-			mode = { "n", "v", "x" },
-		},
-		{
-			"<leader><leader>r",
-			"<cmd>AvanteRefresh<cr>",
-			desc = "Avante - Refresh",
-			mode = { "n", "v", "x" },
-		},
-		{
-			"<leader><leader>f",
-			"<cmd>AvanteFocus<cr>",
-			desc = "CopilotChat - Quick chat",
 			mode = { "n", "v", "x" },
 		},
 		{
