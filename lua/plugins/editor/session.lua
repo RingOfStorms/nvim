64cd348aab58a48da01a3bb51138e86c0cbc3b35
Joshua Bell <abc@joshuabell.xyz>
2026-04-22 10:20:31 -0500

    remove indexing in kde

--- lua/plugins/editor/session.lua
+++ lua/plugins/editor/session.lua
@@ -15,9 +15,39 @@ return {
 	},
 	config = function(_, opts)
 		require("auto-session").setup(opts)
-		-- Auto save on quit
+
+		local diffview_filetypes = {
+			DiffviewFiles = true,
+			DiffviewFileHistory = true,
+			DiffviewFHOptionPanel = true,
+		}
+
+		local function cleanup_for_session()
+			-- Close every tab except the first (reverse order keeps indices valid)
+			local tabs = vim.api.nvim_list_tabpages()
+			for i = #tabs, 2, -1 do
+				local tabnr = vim.api.nvim_tabpage_get_number(tabs[i])
+				pcall(vim.cmd, "tabclose " .. tabnr)
+			end
+
+			-- Wipe leftover diffview buffers so they don't get persisted
+			for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+				if vim.api.nvim_buf_is_valid(bufnr) then
+					local ft = vim.bo[bufnr].filetype
+					local name = vim.api.nvim_buf_get_name(bufnr)
+					if diffview_filetypes[ft] or name:match("^diffview://") then
+						pcall(vim.api.nvim_buf_delete, bufnr, { force = true })
+					end
+				end
+			end
+		end
+
+		-- Auto save on quit (after closing extra tabs so they don't persist)
+		local group = vim.api.nvim_create_augroup("myconfig-auto-session-quit", { clear = true })
 		vim.api.nvim_create_autocmd("QuitPre", {
+			group = group,
 			callback = function()
+				pcall(cleanup_for_session)
 				vim.cmd("SessionSave")
 			end,
 		})
